4713c1340287964d0c91a1604baf22134ed2037b
weslleyrufino <weslleyrufino@hotmail.com>
2026-04-08 23:25:20 +0100

    Recapitulação para fixação do entendimento.

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {cva, type VariantProps} from "class-variance-authority";
 
-
+// O que está dentro de "cva("font-sans text-gray-400"," são os estilos base.
 export const textVariants = cva("font-sans text-gray-400", {
     variants: {
         variant:{
@@ -18,17 +18,39 @@ export const textVariants = cva("font-sans text-gray-400", {
 interface TextProps extends VariantProps<typeof textVariants> {
     // Define qual tag HTML será usada. O keyof React.JSX.IntrinsicElements garante que só tags válidas sejam usadas. 
     // Exemplos válidos: "span"; "p"; "h1";"div"
+    // <Text as="p" />
+    // <Text as="h1" />
+    // <Text as="span" />
     as?: keyof React.JSX.IntrinsicElements;
     className?: string;
+
+    // Define o que vai dentro do componente:
+    // Por exemplo: <Text>Olá mundo</Text>
+    // "Olá mundo" é o children.
+
+    // Com o React.ReactNode ele aceita tudo que o React consegue renderizar, como:
+    // string → "Olá"
+    // number → 123
+    // JSX → <span>Oi</span>
+    // array → [<div />, <div />]
+    // null / undefined / boolean
     children?: React.ReactNode;
+    // Se eu passasse um "children?: string", ele permitiria apenas um <Text>Olá mundo</Text>, mas não um:
+    //  <Text><span>Olá</span><span>Mundo</span></Text>
+    // OU
+    // <Text> {true && <span>Oi</span>}</Text>
+    // OU
+    // <Text> <strong>Olá</strong></Text>
+    // OU
+    // <Text> {undefined} </Text>
 }
 
 export default function Text({
-    as = "span",// Passei "span" pois precisa ter um valor padrão.
-    variant,
+    as = "span",// Qual tag irá usar. Passei "span" pois precisa ter um valor padrão.
+    variant,    // Variantes de estilo, como "body-sm-bold", "body-md" e "body-md-bold"
     className, // Classes CSS (normal do React)
     children, // Conteúdo dentro do componente. Exemplo: <Text>Olá mundo</Text>
-    ...props // Pega qualquer outra propriedade que não esteja dentro do "as";
+    ...props // Pega qualquer outra propriedade que não esteja dentro do "as" (ex: id, onClick, etc.);
 }: TextProps){
     return React.createElement(
         as,
@@ -38,4 +60,11 @@ export default function Text({
         },
         children
     );
-}// Parei em 
+}
+// Resumão final
+// cva → cria variações de estilo
+// VariantProps → tipa automaticamente o variant
+// as → permite trocar a tag HTML dinamicamente
+// children → conteúdo interno do componente
+// createElement → cria o elemento manualmente
+// children fora do {} → porque não é prop, é argumento separado
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// O ! indica que temos certeza que o elemento existe, evitando erros de tipo
+// StrictMode é uma ferramenta de desenvolvimento que ajuda a identificar problemas no código, como ciclos infinitos, uso de APIs obsoletas e outros erros comuns. 
+// Ele não afeta o comportamento da aplicação em produção, mas é muito útil durante o desenvolvimento para garantir que o código seja mais robusto e livre de bugs.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// Resumo:
+
+// StrictMode → ajuda a encontrar bugs (dev only)
+// createRoot → nova forma de renderizar React
+// index.css → estilos globais
+// App → componente principal
+// getElementById('root') → ponto de entrada no HTML
+// .render() → inicia a aplicação
