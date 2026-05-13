bb9e53eac3a17d90e3d4ec10c0af35944ddc2539
weslleyrufino <weslleyrufino@hotmail.com>
2026-05-13 22:54:54 +0100

    Criação componente TaskList.

---
+++ src/core-components/tasks-list.tsx
@@ -0,0 +1,21 @@
+import Button from "../components/button";
+import PlusIcon from "../assets/icons/plus.svg?react";
+import TaskItem from "./task-item";
+
+export default function TasksList(){
+    return (
+        <>
+            <section>
+                <Button icon={PlusIcon} className="w-full">Nova Tarefa</Button>
+            </section>
+
+            <section className="space-y-2">
+                <TaskItem />
+                <TaskItem />
+                <TaskItem />
+                <TaskItem />
+                <TaskItem />
+            </section>
+        </>
+    );
+}
