d7f21d9e3d147d3f61c2c4716a1e20d0bc9ba88c
weslleyrufino <weslleyrufino@hotmail.com>
2026-05-13 21:53:49 +0100

    componente resumo das tarefas.

---
+++ src/core-components/tasks-summary.tsx
@@ -0,0 +1,23 @@
+import Badge from "../components/badge";
+import Text from "../components/text";
+
+export default function TasksSummary(){
+    return (
+        <>
+            <div className="flex items-center gap-2">
+                <Text variant="body-sm-bold" className="!text-gray-300">
+                    Tarefas Criadas
+                </Text>
+                <Badge variant="secondary">5</Badge>
+            </div>
+
+            <div className="flex items-center gap-2">
+                <Text variant="body-sm-bold" className="!text-gray-300">
+                    Concluídas
+                </Text>
+                <Badge variant="primary">2 de 5</Badge>
+            </div>
+
+        </>
+    );
+}
