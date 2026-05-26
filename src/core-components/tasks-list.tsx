0891130058c7d084a1c15470adebf7032f26bd4d
weslleyrufino <weslleyrufino@hotmail.com>
2026-05-26 19:25:41 +0100

    Preparando LocalStorage como banco de dados.

--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -1,8 +1,12 @@
 import Button from "../components/button";
 import PlusIcon from "../assets/icons/plus.svg?react";
 import TaskItem from "./task-item";
+import useTasks from "../hooks/use-tasks";
 
 export default function TasksList(){
+    // const {tasks} = useTasks();
+    // console.log(tasks);
+
     return (
         <>
             <section>
---
+++ src/models/task.ts
@@ -0,0 +1,15 @@
+export const TASKS_KEY = "tasks";
+
+export const TaskState = {
+    Creating: 'creating',
+    Created: 'created',
+} as const;
+
+export type TaskState = typeof TaskState[keyof typeof TaskState];
+
+export interface Task {
+    id: string;
+    title: string;
+    concluded?: boolean;
+    state?: TaskState
+}
