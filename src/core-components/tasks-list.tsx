214568767cb5c1f3f11f5190d998063448d2de19
weslleyrufino <weslleyrufino@hotmail.com>
2026-05-26 21:47:57 +0100

    Preparar tarefa

--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -2,15 +2,24 @@ import Button from "../components/button";
 import PlusIcon from "../assets/icons/plus.svg?react";
 import TaskItem from "./task-item";
 import useTasks from "../hooks/use-tasks";
+import useTask from "../hooks/use-task";
 
 export default function TasksList(){
     const {tasks} = useTasks();
+    const {prepareTask} = useTask();
+
     console.log(tasks);
+    
+    function handleNewTask(){
+        prepareTask();
+    }
 
     return (
         <>
             <section>
-                <Button icon={PlusIcon} className="w-full">Nova Tarefa</Button>
+                <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>
+                    Nova Tarefa
+                </Button>
             </section>
 
             <section className="space-y-2">
---
+++ src/hooks/use-task.ts
@@ -0,0 +1,29 @@
+import LocalStorageLib from "use-local-storage";
+import { TASKS_KEY, TaskState, type Task } from "../models/task";
+
+
+type UseLocalStorage = <T>(
+  key: string,
+  defaultValue: T,
+  options?: unknown
+) => [T, (value: T) => void, () => void];
+
+const useLocalStorage = (
+  LocalStorageLib as unknown as { default: UseLocalStorage }
+).default;
+
+export default function useTask() {
+    const [tasks, setTasks] = useLocalStorage<Task[]>(TASKS_KEY, []);
+
+    function prepareTask(){
+        setTasks([...tasks, {
+            id: Math.random().toString(36).substring(2, 9),
+            title: "",
+            state: TaskState.Creating
+        }]);
+    }
+
+    return {
+        prepareTask
+    }
+}
